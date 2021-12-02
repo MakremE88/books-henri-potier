974dea2d510ae4ebdfbc01861f47fbf19a1272ad
MakremE88 <ellouzemakrem@gmail.com>
2021-12-02 22:42:49 +0100

    Project Book

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,29 +4,32 @@ import { Link } from "react-router-dom";
 import LocalMallIcon from '@mui/icons-material/LocalMall';
 import CartSummary from "./CartSummary";
 
+
 function Header() {
-  return (
-    <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
+    return (
+        <>
+  <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
           <Typography
             noWrap
             color="primary"
             component="div"
             sx={{ flexGrow: 1, display: { sm: 'block' } }}
           >
-            <Button variant="h3">
-              <Link className="button-header" to="/Book-Henri-Potier/">La bibliothèque d'Henri Potier</Link>
-            </Button>
+          <Button variant="h3">
+            <Link className="button-header" to="/Book-Henri-Potier/">La bibliothèque d'Henri Potier</Link>
+          </Button>
           </Typography>
-            <Link to="/Book-Henri-Potier/cart" className="flex-cart" variant="h3">
+          <Link to="/Book-Henri-Potier/cart" className="flex-cart" variant="h3">
               <LocalMallIcon name="cart" size="small" /> <CartSummary />
-            </Link>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </>
-  )
+          </Link>
+          
+        </Toolbar>
+      </AppBar>
+    </Box>
+</>
+    )
 }
-export default Header;
+
+export default Header
